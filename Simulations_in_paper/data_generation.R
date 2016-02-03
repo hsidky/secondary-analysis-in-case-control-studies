51009c5a260f41d807d70555f3b5bfbd05d17fac
Xiaoyu Song <xs2148@cumc.columbia.edu>
2016-02-03 10:42:25 -0500

    updates

--- Simulations_in_paper/data_generation.R
+++ Simulations_in_paper/data_generation.R
@@ -1,12 +1,12 @@
-# This code serves as generating the simulation data. It considers six scenarios of Logistic Setting, Interaction Setting and Piecewise Linear Setting of binary and continuous secondary trait.
-# It includes a continuous covariates Z. 
+# This code serves as generating the simulation data set. It includes Logistic Setting, Interaction Setting and Piecewise Linear Setting of binary and continuous secondary trait under random sample. It also gives the example of convenient sample and stratified sample.
+# It includes a continuous covariates Z. (The covariates Z is deleted in simulations of type I error and power comparisons in the paper, to accelerate the computation of 100,000 simulations in each scenario.)
 
 
 
 
-rm(list=ls())
-set.seed(123)
-setwd(dir)
+rm(list=ls()) 
+set.seed(123) 
+setwd(dir) # set workding directory to save the data. The data has to be saved, as the SPML is performed in a separete software requiring individual data set.
 #---------------- Logistic Setting with Binary outcome -----------------#
 
 # population of 500,000 subjects
@@ -280,3 +280,114 @@ for (i in 1:500) {
 }
 
 
+
+
+#-------- (Example of convenient sampling scheme: Bbasecase with unmeasured confounding factor) --------#
+#set.seed(123)
+set.seed(127)
+#set.seed(521)
+# population 
+n=500000 #(500,000)
+
+# parameters
+beta=c(0.2, 0.1) # P(Y|X,Z)
+gamma=c(0.3, log(2), log(2)) #P(D|X,Y,Z)
+# generate X
+x = rbinom(n,size=2,prob=0.3)
+
+# generate Z
+z=rnorm(n, mean=0.5*x-0.3, sd=1)
+
+# generate Y
+py=exp(-1+beta[1]*x+beta[2]*z)/(1+exp(-1+beta[1]*x+beta[2]*z))
+y=rbinom(n,1, py)
+
+# generate the unmeasured counfounding factor v
+alpha=-1
+pv = exp(alpha+ x*gamma[1] + y*log(2)+ z*log(2)) / (1+exp(alpha+ x*gamma[1] + y*log(2)+ z*log(2)))
+v=rbinom(n, 1, pv)
+
+# generate D
+alpha = -2.88 #basecase
+#alpha=-2.95 # interaction
+pd = exp(alpha+ x*gamma[1] + y*log(2)+ z*log(2)) / (1+exp(alpha+ x*gamma[1] + y*log(2)+ z*log(2))) # basecase
+#pd = exp(alpha+ x*gamma[1] + y*log(2)+ z*log(2)+0.2*x*y) / (1+exp(alpha+ x*gamma[1] + y*log(2)+ z*log(2)+0.2*x*y)) # interaction
+#temp=0.3*x+log(2)*y+log(2)*z #piecewsie
+#qt=quantile(temp)
+#pd=ifelse(temp<qt[2], 0.05, ifelse(temp<qt[4], (temp-qt[2])/(qt[4]-qt[2])*0.1+0.05, 0.15))
+d=rbinom(n, 1, pd)
+
+# population data
+dat=as.data.frame(cbind(d, y, z, x, v))
+
+# 500 samples
+dat_cases = dat[which(dat$d==1 & dat$v==1),]
+dat_controls= dat[which(dat$d==0 & dat$v==1),]
+for (i in 1:500) {
+	print(i)
+	dat_cases_sample = dat_cases[sample(sum(dat$d==1 & dat$v==1),2000, replace=F),]
+	dat_controls_sample = dat_controls[sample(sum(dat$d==0 & dat$v==1),2000, replace=F),]
+	sample=rbind(dat_cases_sample,dat_controls_sample)
+	colnames(sample)[1]=c("D")
+	sample=sample[,c("D", "y", "z", "x" )]
+	# sample2=t(as.matrix(sample))	
+	write.table(sample2, file=paste("CBpiecewise/", i, ".txt", sep=""), col.names=F, row.names=F)
+	
+}
+
+
+#-------- (Example of stratified sampling scheme: Bbasecase with oversample obs) --------#
+# population size 
+n=500000 #(500,000)
+
+# parameters
+beta=c(0.2, 0.1) # P(Y|X,Z)
+gamma=c(0.3, log(2), log(2)) #P(D|X,Y,Z)
+
+
+# generate X -- genetic marker (additive model)
+x = rbinom(n,size=2,prob=0.3)
+
+# generate Z -- covariate
+z=rnorm(n, mean=0.5*x-0.3, sd=1)
+
+# weights 
+w=rnorm(n)
+
+# generate Y -- secondary outcome 
+py=exp(-1+beta[1]*x+beta[2]*z)/(1+exp(-1+beta[1]*x+beta[2]*z))
+y=rbinom(n,1, py)
+
+# generate d -- primary disease
+alpha= -2.88
+pd = exp(alpha+ x*gamma[1] + y*log(2)+ z*log(2)) / (1+exp(alpha+ x*gamma[1] + y*log(2)+ z*log(2)))
+d = rbinom(n,size=1,prob=pd)
+mean(pd)
+
+# population dataset
+dat = as.data.frame(cbind(d, y, z, x, w))
+
+
+# generate 500 Monta-Carlo dataset with 2000 cases and 2000 controls from the population
+dat_cases1 = dat[which(dat$d==1 & dat$w>0),]
+dat_cases2 = dat[which(dat$d==1 & dat$w<0),]
+dat_controls1= dat[which(dat$d==0 & dat$w>0),]
+dat_controls2= dat[which(dat$d==0 & dat$w<0),]
+
+for (i in 1:500) {
+	print(c(i))
+	dat_cases_sample1 = dat_cases1[sample(sum(dat$d==1 & dat$w>0),1800, replace=F),]
+	dat_cases_sample2 = dat_cases2[sample(sum(dat$d==1 & dat$w<0),200, replace=F),]
+	dat_controls_sample1 = dat_controls1[sample(sum(dat$d==0 & dat$w>0),1800, replace=F),]
+	dat_controls_sample2 = dat_controls2[sample(sum(dat$d==0 & dat$w<0),200, replace=F),]
+
+	sample= as.matrix(rbind(dat_cases_sample1,dat_cases_sample2,dat_controls_sample1,dat_controls_sample2))
+	sample1=sample[, 1:4]
+	sample2=t(sample)
+	sample3=t(sample1)
+	string1=paste("WBbasecase/input", i, ".txt", sep="")
+	string2=paste("WBbasecase/input_w", i, ".txt", sep="")
+	write.table(sample2, string2, col.names=F, row.names=F)
+	write.table(sample3, string1, col.names=F, row.names=F)
+
+}
